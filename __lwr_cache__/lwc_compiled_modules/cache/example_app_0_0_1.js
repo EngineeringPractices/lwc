3dbaeec9a1c927a491a3d4df869369e87d5076ad
sunwei <me@sunwei.xyz>
2024-06-11 14:02:05 +0800

    first commit

---
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_0_0_1.js
@@ -0,0 +1,10 @@
+import _tmpl from "./app.html";
+import { LightningElement, registerComponent as _registerComponent } from 'lwc';
+class HelloWorldApp extends LightningElement {
+  /*LWC compiler v6.6.6*/
+}
+export default _registerComponent(HelloWorldApp, {
+  tmpl: _tmpl,
+  sel: "example-app",
+  apiVersion: 62
+});
